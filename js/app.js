a86cd718795686d955a1ea861e2e895432b280f0
mowuj <mowuj92@gmail.com>
2022-03-01 21:41:22 +0600

    more info

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ const searchPhone = () => {
 const displayPhone = phones => {
     // console.log(phones)
     const container = document.getElementById('phone');
-    // container.textContent = '';
+    container.textContent = '';
     phones.forEach(phone => {
         // console.log(phone.slug)
         const div = document.createElement('div');
@@ -47,6 +47,7 @@ const loadPhoneDetail = phoneId => {
 const displayPhoneDetails = phone => {
     console.log(phone)
     const phoneDetails = document.getElementById('phone-detils');
+    phoneDetails.textContent = '';
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
@@ -56,8 +57,44 @@ const displayPhoneDetails = phone => {
                 <h5 class="card-title">${phone.brand}</h5>
                 <h5 class="card-title">${phone.name}</h5>
                 <h5 class="card-title">${phone.releaseDate}</h5>
-                
+                <button onclick="loadPhoneMoreDetail('${phone.slug}')">More Info</button>
             </div>`;
     phoneDetails.appendChild(div)
+
+}
+const loadPhoneMoreDetail = phoneId => {
+
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneMoreDetails(data.data))
+}
+const displayPhoneMoreDetails = phone => {
+    console.log(phone)
+    const phoneMoreDetails = document.getElementById('more_details');
+    phoneMoreDetails.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add('col-md-8');
+    div.innerHTML = `
+            <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="${phone.image}" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Phone Info</h5>
+                                
+                                <p class="card-text">Brand:${phone.brand}</p>
+                                <p class="card-text">${phone.mainFeatures.storage}</p>
+                                <p class="card-text">${phone.mainFeatures.displaySize}</p>
+                                <p class="card-text">${phone.mainFeatures.chipSet}</p>
+                                <p class="card-text">${phone.mainFeatures.memory}</p>
+                            </div>
+                            <button onclick="loadOtherDetail('${phone.slug}')">More Info</button>
+                        </div>
+                    </div>
+`;
+    
+    phoneMoreDetails.appendChild(div)
 
 }
